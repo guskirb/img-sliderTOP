51ad6cd984fd4a76c09e060f8d6e3d981070d0f1
guskirb <anguskirby1998@gmail.com>
2024-04-18 00:50:46 +0100

    Add button functionality and css effects

--- index.js
+++ index.js
@@ -1,14 +1,49 @@
 const imageBackground = document.querySelector('.imgContainer');
+const prevArrow = document.getElementById('prevArrow');
+const nextArrow = document.getElementById('nextArrow');
 
 const slideshow = ['img/img0.jpeg', 'img/img1.jpeg', 'img/img2.jpg', 'img/img3.jpg', 'img/img4.jpg']
 
-function setImage(value) {
-    imageBackground.style.backgroundImage = `url(${slideshow[value]})`;
-    if (document.getElementsByClassName("active").length === 0) {
-        document.getElementById(`img${value}`).classList.add('active');
+let num = 0;
+
+prevArrow.addEventListener("click", prevSlide);
+nextArrow.addEventListener("click", nextSlide);
+
+function changeSlide() {
+    setImage(num)
+    if (num < slideshow.length - 1) {
+        num++;
     } else {
-        document.querySelector('.active').classList.remove('active');
-        document.getElementById(`img${value}`).classList.add('active');
+        num = 0;
     }
+
+    setTimeout("changeSlide()", 5000);
 }
 
+function setImage(value) {
+    imageBackground.style.backgroundImage = `url(${slideshow[value]})`;
+    document.querySelector('.active')?.classList.remove('active');
+    document.getElementById(`img${value}`).classList.add('active');
+}
+
+function nextSlide() {
+    if (num < slideshow.length - 1) {
+        num++;
+    } else {
+        num = 0;
+    }
+
+    setImage(num)
+}
+
+function prevSlide() {
+    if (num > 0) {
+        num--;
+    } else {
+        num = 4;
+    }
+
+    setImage(num)
+}
+
+window.onload = changeSlide;
